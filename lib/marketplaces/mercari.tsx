a09206361fe39b5c872489cad894af5cc51846d7
Nephtali Rodriguez <nerodriguez@shutterstock.com>
2021-08-08 02:17:03 -0400

    Adds fix for mercari pricing

--- lib/marketplaces/mercari.tsx
+++ lib/marketplaces/mercari.tsx
@@ -33,22 +33,33 @@ export class Mercari implements Marketplace {
     } catch (error) {
       // eslint-disable-next-line no-console
       console.log('Error', error)
+      //We hit a rate limiting error or something, just return no results for now
+      return []
     }
   }
 
-  transformData(rawResults: Array<any>): Array<TransformedItem> {
+  private transformPrice(originalPrice: string) {
     const formatter = new Intl.NumberFormat('en-US', {
       style: 'currency',
       currency: 'USD',
       minimumFractionDigits: 2,
     })
 
+    //Mercari API prices comes back with two extra 00's on them
+    //Remove that first by dividing by 100
+    const fixedPrice = parseInt(originalPrice) / 100
+
+    //Format fixed price to show with proper us currency
+    return formatter.format(fixedPrice)
+  }
+
+  transformData(rawResults: Array<any>): Array<TransformedItem> {
     return rawResults.map((item: MercariItem) => {
       return {
         name: item.name,
         photoUrl: item.photos[0].thumbnail, //.split("?")[0],
-        price: formatter.format(parseInt(item.price)),
-        originalPrice: formatter.format(parseInt(item.originalPrice)),
+        price: this.transformPrice(item.price),
+        originalPrice: this.transformPrice(item.originalPrice),
         brand: item.brand.name,
         merchant: 'mercari',
       }
